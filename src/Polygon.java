1758a5808fa926833831f6632230d086bbc2a6cc
Thomas Mathiassen <thomasmathiassen99@gmail.com>
2023-11-09 15:50:04 +0100

    step 3 currently

--- src/Polygon.java
+++ src/Polygon.java
@@ -8,22 +8,26 @@ public abstract class Polygon extends JComponent{
     Point centerPoint;
 
     private Polygon(Point center){
+
         this.centerPoint = center;
     }
     public Polygon(int x, int y){
+
         this(new Point(x,y));
     }
 
     @Override
     public String toString(){
+
         return "Polygon{}";
     }
 
     // TODO: Step 1a: Understand this method declaration and its helper.
-    public void overlaps(Polygon other){
+    public void overlaps(Polygon other) {
+
         overlapsHelper("Polygon", this.toString(), other.toString());
     }
-    static void overlapsHelper(String className, String thisType, String otherType){
+    static void overlapsHelper(String className, String thisType, String otherType) {
         System.out.println("" + className + ".overlaps: " + thisType + " vs " + otherType);
     }
 
--- src/TestOverlaps.java
+++ src/TestOverlaps.java
@@ -7,19 +7,22 @@ public class TestOverlaps {
 
         // TODO: Step 1c: Predict what the result of these method calls will be.
         //   Fill in the blanks in the comment for each line before running the program.
-        square.overlaps(triangle);      // ___.overlaps:___{} vs ___{}
-        square.overlaps(square);        // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(rectangle);   // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(triangle);    // ___.overlaps:___{} vs ___{}
+
+        /*
+        square.overlaps(triangle);      // Polygon.overlaps: Square{} vs Triangle{}
+        square.overlaps(square);        // Polygon.overlaps: Square{} vs Square{}
+        triangle.overlaps(rectangle);   // Polygon.overlaps: Triangle{} vs Rectangle{}
+        triangle.overlaps(triangle);    // Polygon.overlaps: Triangle{} vs Triangle{}
+        */
 
         // TODO: Step 2b: After you have uncommented all three overridden implementations
         //   of the method 'overlaps' in the subclasses, as per step 2a,
         //   again predict the result of the same method calls above.
         //   Fill in the blanks below before running the program.
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
+        // Square.overlaps: Square{} vs Triangle{}
+        // Square.overlaps: Square{} vs Square{}
+        // Triangle.overlaps: Triangle{} vs Rectangle{}
+        // Triangle.overlaps: Triangle{} vs Triangle{}
 
         // TODO: Step 3: Now we introduce a variable with static type Polygon. What will
         //   the results be this time? Fill in the blanks as before, then uncomment all
@@ -29,7 +32,7 @@ public class TestOverlaps {
         //   You can comment out the lines from above (10-13) using the same command.
         //   Then run the program again, and see if your predictions were correct.
         //   Can you explain these outputs in terms of static and dynamic types?
-        /*
+
         Polygon polyTriangle = new Triangle(1,4);
 
         square.overlaps(polyTriangle);          // ___.overlaps:___{} vs ___{}
@@ -37,7 +40,7 @@ public class TestOverlaps {
         polyTriangle.overlaps(triangle);        // ___.overlaps:___{} vs ___{}
         polyTriangle.overlaps(polyTriangle);    // ___.overlaps:___{} vs ___{}
         triangle.overlaps(triangle);            // ___.overlaps:___{} vs ___{}
-        */
+
 
         // TODO: Step 4b: After you have uncommented the overloaded declaration and
         //   implementation of the method 'overlaps' in the Triangle subclass,
